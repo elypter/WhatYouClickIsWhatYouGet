3872d20c965a527a3e6e778888d096d5eb3acf6f
elypter <undefined@web.de>
2017-12-26 19:35:13 +0100

    changing names and adding additional description

--- whatyouclickiswhatyouget.user.js
+++ whatyouclickiswhatyouget.user.js
@@ -1,14 +1,19 @@
 // ==UserScript==
-// @name WhatIClickIsWhatIGet
-// @namespace whaticlickiswhatiget
+// @name WhatYouClickIsWhatYouGet
+// @namespace whatyouclickiswhatyouget
 // @author elypter
 // @match *://*/*
 // @grant none
-// @version     1
+// @version     2
 // @description Userscript that removes all event listeners and replaces all data-outbound-url attrbutes from links
-// @downloadURL https://raw.githubusercontent.com/elypter/WhatIClickIsWhatIGet/master/whaticlickiswhatiget.user.js
-// @updateURL https://raw.githubusercontent.com/elypter/WhatIClickIsWhatIGet/master/whaticlickiswhatiget.user.js
+// @downloadURL https://raw.githubusercontent.com/elypter/WhatYouClickIsWhatYouGet/master/whatyouclickiswhatyouget.user.js
+// @updateURL https://raw.githubusercontent.com/elypter/WhatYouClickIsWhatYouGet/master/whatyouclickiswhatyouget.user.js
 // ==/UserScript==
+
+//Do you hate it when you hover over a link and it looks completely normal
+//but when you click on it it takes you somewhere else or redirects you over some logging page?
+//this script gets rid of all that fuckery so that what you click is what you get.
+//Userscript that removes all event listeners and replaces all data-outbound-url attrbutes from links
 
 //this script isnt particularly lean on performance but nothing to worry about. it took over 100ms on my over 10 year old laptop
 
